3c51989c3931293b56acdda5be5308a7d1437c78
jeongwon6955 <choirio2467@gmail.com>
2025-11-12 14:56:26 +0900

    .

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,6 @@
+// 버튼 스크롤 효과
 const introBtn = document.querySelector('#intro_btn');
+const methodBtn = document.querySelector('#method_btn');
 
 introBtn.addEventListener('click', () => {
     const target = document.querySelector('.intro');
@@ -8,3 +10,30 @@ introBtn.addEventListener('click', () => {
         block: 'end'
     })
 })
+
+methodBtn.addEventListener('click', () => {
+    const target = document.querySelector('.method');
+
+    target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end'
+    })
+})
+
+// 스크롤 효과
+const containers = document.querySelectorAll('.container');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+       if (entry.isIntersecting) {
+         entry.target.classList.add('show');
+       } else {
+         // 화면에서 사라지면 다시 숨기고 싶다면 ↓ 이 줄 활성화
+         entry.target.classList.remove('show');
+       }
+  });
+}, {
+  threshold: 0.2  // 요소가 20% 보일 때 발동
+});
+
+containers.forEach(container => observer.observe(container));
